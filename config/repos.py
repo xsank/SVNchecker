39414d75454a3ab523f1da3fbc2709e6b3dc4be4
xsank <825128975@qq.com>
2013-10-19 18:00:52 +0800

    功能完善

---
+++ config/repos.py
@@ -0,0 +1,17 @@
+'''
+Created on 2013-10-16
+
+@author: xsank
+'''
+
+#it is too bad to add all project in one repos
+
+#infact my_repos should be "" if you create repos properly
+MY_PROJECT="webp2p/flashp2p/DarkForrest/"
+CHECK_DIRECTORY_DICT={
+      "code":"assets,code",
+      "config":"config.version,language",
+      #"document":"document",
+      #"resource":"res",
+                      }
+
